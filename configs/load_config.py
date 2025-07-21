aa15a899585659af71e82008a9749da393d1dbd2
Yash Kushwaha <yash1158@gmail.com>
2025-07-21 21:27:52 +0530

    minor changes

--- configs/load_config.py
+++ configs/load_config.py
@@ -2,7 +2,7 @@ import configparser
 import os
 
 config = configparser.ConfigParser()
-config.read('infra_config.ini')
+config.read('configs/infra_config.ini')
 
 # Write to GitHub environment file
 github_env = os.environ.get('GITHUB_ENV', 'temp.txt')
@@ -15,6 +15,7 @@ with open(github_env, 'a') as env_file:
     for section in config.sections(): 
         for key, val in dict(config.items(section)).items():
             _ = f'{section}_{key}'.upper()
+            print(_)
             env_file.write(f"{_}={val}\n")
 
 print(f"Wrote to {github_env}:")
