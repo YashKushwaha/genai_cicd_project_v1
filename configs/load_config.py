56c67cb9fb8d68238c305720e64776e771932d51
Yash Kushwaha <yash1158@gmail.com>
2025-07-23 11:11:00 +0530

    push docker image to new ecr repo

--- configs/load_config.py
+++ configs/load_config.py
@@ -8,9 +8,6 @@ config.read('configs/infra_config.ini')
 github_env = os.environ.get('GITHUB_ENV', 'temp.txt')
 print('github_env -> ', github_env)
 
-with open(github_env, 'r') as env_file:
-    print(env_file)
-
 with open(github_env, 'a') as env_file:
     for section in config.sections(): 
         for key, val in dict(config.items(section)).items():
